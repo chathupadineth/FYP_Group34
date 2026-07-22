6c303f901bd7b7596fba14830b49b61e2b6a4be9
Chathupa <chathupadineth@gmail.com>
2026-07-23 02:25:17 +0530

    Initial commit: jet_auto_simulation package with walls and obstacles

---
+++ src/jet_auto_simulation/launch/jet_world.launch.py
@@ -0,0 +1,18 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('jet_auto_simulation')
+    world_path = os.path.join(pkg_share, 'worlds', 'jet_world.sdf')
+
+    gazebo = ExecuteProcess(
+        cmd=['ign', 'gazebo', world_path],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        gazebo
+    ])
